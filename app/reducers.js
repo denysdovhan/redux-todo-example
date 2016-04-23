7e207382d6f6d8ce991dd2b3603584fef91cd14b
Denys Dovhan <email@denysdovhan.com>
2016-04-23 19:00:14 +0300

    Add reducers.js.

---
+++ app/reducers.js
@@ -0,0 +1,50 @@
+import { combineReducers } from 'redux';
+import {
+  ADD_TODO,
+  COMPLETE_TODO,
+  SET_VISIBILITY_FILTER,
+  visibilityFilters
+} from './action';
+
+const { SHOW_ALL } = visibilityFilters;
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        {
+          text: action.text,
+          completed: false
+        },
+        ...state
+      ]
+    case COMPLETE_TODO:
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          return Object.assign(
+            {},
+            todo,
+            { completed: true }
+          )
+        }
+      })
+    default:
+      return state;
+  }
+}
+
+function visibilityFilter(state = SHOW_ALL, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter;
+    default:
+      return state;
+  }
+}
+
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+});
+
+export default todoApp;
