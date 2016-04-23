a9965b32e17f2bfdb2a76927661eb614e2f71daf
Denys Dovhan <email@denysdovhan.com>
2016-04-23 19:28:32 +0300

    Add store.

--- app/index.js
+++ app/index.js
@@ -1,3 +1,15 @@
-const name = 'World';
+import { createStore } from 'redux';
+import todoApp from './reducers';
+import { addTodo, completeTodo, setVisibilityFilter, visibilityFilters } from './action';
 
-console.log(`Hello, ${name}!`);
+let store = createStore(todoApp);
+
+console.log(store.getState());
+
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
+
+store.dispatch(addTodo('lol'));
+store.dispatch(setVisibilityFilter(visibilityFilters.SHOW_DONE));
+store.dispatch(completeTodo(0));
+
+unsubscribe();
